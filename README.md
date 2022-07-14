8b0f957e3eed72d356c29fb0f99e3d75fb0b3a1f
Mihai Simion <mihai@simion.dev>
2022-07-14 15:08:28 +0300

    updated Readme, refactored code to be more readable

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 # tennis
-# This project controls tennis game scores and shows feedback on how the score looks currently
+# Write the code (implementation + tests) in clojure giving ability to report score of single set of a tennis match in REPL. Code should handle state between subsequent calls of functions in REPL. After REPL is stopped state don't have to be stored anymore.
 
+# Example interaction with REPL:
+```(tennis-set-simulator)
+0 : 0
+(tennis-set-simulator :player-1-scored)
+15 : 0
+(tennis-set-simulator :player-1-scored)
+30 : 0
+(tennis-set-simulator :player-2-scored)
+30 : 15
+(tennis-set-simulator :player-1-scored)
+40 : 15
+(tennis-set-simulator :player-1-scored)
+WON! : 15
+(tennis-set-simulator)
+WON! : 15
+(tennis-set-simulator :reset)
+0 : 0
+(tennis-set-simulator)
+0 : 0
+```
+# extended the task and so I implemented not only point and set but also: match, game.
+
+
+# This project contains a simulation which controls tennis game scores and shows feedback on how the score looks currently
 ## Run game simulation:
 ```
 clj -M:run
@@ -22,6 +46,6 @@ Available functions:
 ```
 (tennis-set-score :player1) ;; add game to player1 
 (tennis-set-score :player2) ;; add game to player2
-(tennis-set-score :random)  ;; add game to random player
+(tennis-set-score :random-player)  ;; add game to random player
 (tennis-set-score :reset)   ;; reset game
 ```
--- src/tennis/core.clj
+++ src/tennis/core.clj
@@ -3,50 +3,68 @@
 (def config {:max-matches 3
              :labels [0 15 30 40 "A"]})
 
+(defn label [i]
+  (nth (:labels config) i))
 
-(def default-scores {:player1 [0 [0] 0]
-                     :player2 [0 [0] 0]})
+(def default-scores {:player1 {:point 0
+                               :sets [0]
+                               :match 0}
+                     :player2 {:point 0
+                               :sets [0]
+                               :match 0}})
 
 (def scores (atom default-scores))
 
 
-(defn inc-game-only?
-  "when winning a game increases only the game score"
-  [game game-adv]
-  (or (< game 3)
-      (= game game-adv)))
+(defn- adversary-advantage?
+  "when adversary has advantage, then it defaults to 40-40(deuce) for both players"
+  [point point-adv]
+  (= (- point point-adv) -1))
+
+(defn- inc-point-only?
+  "when winning a point increases only the point score"
+  [point point-adv]
+  (or (< point 3)
+      (= point point-adv)))
 
 
-(defn inc-set-match?
-  "when winning a game leads to increase set and match scores"
-  [game game-adv set set-adv]
-  (and (>= (- game game-adv) 1)
+(defn- inc-set-match?
+  "when winning a point leads to increase set and match scores"
+  [point point-adv set set-adv]
+  (and (>= (- point point-adv) 1)
        (>= set 5)
        (>= (- set set-adv) 1)))
 
 
-(defn inc-set-only?
-  "when winning a game increases only set score"
-  [game game-adv set set-adv]
-  (and (>= game 3)
-       (>= (- game game-adv) 1)
-       (or (< set 6)
-           (= (- set set-adv) -1)
-           (= (- set set-adv) 0))))
+(defn- inc-set-only?
+  "when winning a point increases only set score"
+  [point point-adv set set-adv]
+  (and
+   (not (inc-set-match? point point-adv set set-adv))
+   (>= point 3)
+   (>= (- point point-adv) 1)
+   (or (< set 6)
+       (= (- set set-adv) -1)
+       (= (- set set-adv) 0))))
 
 
-(defn adversary-advantage?
-  "when adversary has advantage, then it defaults to 40-40(deuce) for both players"
-  [game game-adv]
-  (= (- game game-adv) -1))
+(defn- inc-set [sets set end-match?]
+  (if end-match?
+    (conj (pop sets) (inc set))
+    (conj (pop sets) (inc set) 0)))
 
 
 (defn increase-score
+  "Increase one of the player's score
+   with one argument - convenient to serve the game,
+   with two arguments - useful to test specific state change."
   ([player] (increase-score player @scores))
   ([player score]
-   (let [[game sets match] (player score)
-         [adversary [game-adv sets-adv match-adv]] (->> (remove #(= (key %) player) score)
-                                                        (first))
+   (let [{:keys [point sets match]} (player score)
+         [adversary {point-adv :point
+                     sets-adv :sets
+                     match-adv :match}] (->> (remove #(= (key %) player) score)
+                                             (first))
          set (last sets)
          set-adv (last sets-adv)
          {:keys [max-matches]} config]
@@ -54,36 +72,37 @@
        (>= (max match match-adv) max-matches)
        score
 
-         ;;when winning a game increases only the game score 
-       (inc-game-only? game game-adv)
-       {player [(inc game) sets match]
-        adversary [game-adv sets-adv match-adv]}
+       ;;when adversary has advantage, then it defaults to 40-40(deuce) for both players
+       (adversary-advantage? point point-adv)
+       (-> score
+           (assoc-in [player :point] 3)
+           (assoc-in [adversary :point] 3))
 
-         ;;when winning a game increases set and match scores
-       (inc-set-match? game game-adv set set-adv)
+       ;;when winning a point increases only the point score 
+       (inc-point-only? point point-adv)
+       (assoc-in score [player :point] (inc point))
+
+       ;;when winning a point increases only set score 
+       (inc-set-only? point point-adv set set-adv)
+       (-> score
+           (assoc-in [player :point] 0)
+           (assoc-in [player :sets] (conj (pop sets) (inc set)))
+           (assoc-in [adversary :point] 0))
+
+       ;;when winning a point increases set and match scores
+       (inc-set-match? point point-adv set set-adv)
        (let [end-match? (= (inc match) max-matches)]
-         (if end-match?
-           {player [game (conj (pop sets) (inc set)) (inc match)]
-            adversary [game-adv (conj (pop sets-adv) set-adv) match-adv]}
+         (-> score
+             (assoc-in [player :point] (if end-match? point 0))
+             (assoc-in [player :sets] (inc-set sets set end-match?))
+             (assoc-in [player :match] (inc match))
+             (assoc-in [adversary :point] (if end-match? point-adv 0))
+             (assoc-in [adversary :sets] (if end-match? sets-adv (conj sets-adv 0)))))))))
 
-           {player [0 (conj (pop sets) (inc set) 0) (inc match)]
-            adversary [0 (conj (pop sets-adv) set-adv 0) match-adv]}))
-
-
-         ;;when winning a game increases only set score 
-       (inc-set-only? game game-adv set set-adv)
-       {player [0 (conj (pop sets) (inc set)) match]
-        adversary [0 sets-adv match-adv]}
-
-         ;;when adversary has advantage, then it defaults to 40-40(deuce) for both players
-       (adversary-advantage? game game-adv)
-       {player [3 sets match]
-        adversary [3 sets-adv match-adv]}))))
-
-(defn print-score [new-state]
-  (let [{:keys [labels max-matches]} config
-        [game1 sets1 match1] (:player1 new-state)
-        [game2 sets2 match2] (:player2 new-state)]
+(defn- print-total-score [new-state]
+  (let [{:keys [max-matches]} config
+        {point1 :point sets1 :sets match1 :match} (:player1 new-state)
+        {point2 :point sets2 :sets match2 :match} (:player2 new-state)]
     (if (>= (max match1 match2) max-matches)
       (println (if (= match1 max-matches)
                  "PLAYER 1"
@@ -93,22 +112,35 @@
               ... or to reset the game use command (tennis-set-score :reset)")
       (do
         (println "PLAYER 1   |   PLAYER 2")
-        (println "Game:  " (nth labels game1) " | "
-                 (nth labels game2))
+        (println "point:  " (label point1) " | "
+                 (label point2))
         (println "Set:   " (last sets1) " | " (last sets2))
         (println "Match: " match1 " | " match2)
         (println "All sets: " (map vector sets1 sets2))))))
 
+(defn- print-points-score [old-state new-state]
+  (let [{old-point1 :point} (:player1 old-state)
+        {old-point2 :point} (:player2 old-state)
+        {point1 :point} (:player1 new-state)
+        {point2 :point} (:player2 new-state)]
+    (if (and (= (max point1 point2) 0)
+             (> (max old-point1 old-point2) 0))
+      (if (> old-point1 old-point2)
+        (println (str "WON! : " (label old-point2)))
+        (println (str (label old-point1) " : WON!")))
+      (println (label point1) " : " (label point2)))))
+
 (add-watch scores :watcher
-           (fn [_ _ _ new-state]
-             (print-score new-state)))
+           (fn [_ _ old-state new-state]
+             (print-total-score new-state)
+             (print-points-score old-state new-state)))
 
 
-(defn reset-score [score]
+(defn- reset-score [score]
   (reset! scores score))
 
 
-(defn add-game-to [player]
+(defn- add-point-to [player]
   (->> (increase-score player)
        (reset! scores)))
 
@@ -116,16 +148,16 @@
 (defn tennis-set-simulator [player]
   (case player
     :reset (reset-score default-scores)
-    :random (add-game-to (rand-nth [:player1 :player2]))
-    :player-1-scored (add-game-to :player1)
-    :player-2-scored (add-game-to :player2)))
+    :random-player (add-point-to (rand-nth [:player1 :player2]))
+    :player-1-scored (add-point-to :player1)
+    :player-2-scored (add-point-to :player2)))
 
 
 (defn -main [& _]
   (let [{:keys [max-matches]} config]
     (loop [state (tennis-set-simulator :reset)]
-      (let [match1 (last (:player1 state))
-            match2 (last (:player2 state))]
-        (Thread/sleep 500)
+      (let [match1 (:match (:player1 state))
+            match2 (:match (:player2 state))]
+        (Thread/sleep 300)
         (when (< (max match1 match2) max-matches)
-          (recur (tennis-set-simulator :random)))))))
+          (recur (tennis-set-simulator :random-player)))))))
